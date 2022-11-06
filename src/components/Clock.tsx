a76e79697b7a6e46af46d4dfba47014046bb7f69
tatumaru <42592450+tatumaru@users.noreply.github.com>
2022-11-06 22:56:22 +0900

    テキストのバグを修正

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,63 +1,63 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
-const UPDATE_CYCLE = 1000
-const KEY_LOCALE = 'KEY_LOCALE'
+const UPDATE_CYCLE = 1000;
+const KEY_LOCALE = 'KEY_LOCALE';
 
 enum Locale {
-    US = 'en_US',
-    JP = 'ja_JP'
+  US = 'en-US',
+  JP = 'ja-JP',
 }
 
 const getLocaleFromString = (text: string) => {
-    switch (text) {
-        case Locale.US:
-            return Locale.US
-        case Locale.JP:
-            return Locale.JP
-        default:
-            return Locale.US
-    }
-}
+  switch (text) {
+    case Locale.US:
+      return Locale.US;
+    case Locale.JP:
+      return Locale.JP;
+    default:
+      return Locale.US;
+  }
+};
 
 export const Clock = () => {
-    const [timestamp, setTimestamp] = useState(new Date())
-    const [locale, setLocale] = useState(Locale.US)
+  const [timestamp, setTimestamp] = useState(new Date());
+  const [locale, setLocale] = useState(Locale.US);
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTimestamp(new Date())
-        }, UPDATE_CYCLE)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimestamp(new Date());
+    }, UPDATE_CYCLE);
 
-        return () => {
-            clearInterval(timer)
-        }
-    }, [])
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
-    useEffect(() => {
-        const savedLocale = localStorage.getItem(KEY_LOCALE)
-        if (savedLocale !== null) {
-            setLocale(getLocaleFromString(savedLocale))
-        }
-    }, [])
+  useEffect(() => {
+    const savedLocale = localStorage.getItem(KEY_LOCALE);
+    if (savedLocale !== null) {
+      setLocale(getLocaleFromString(savedLocale));
+    }
+  }, []);
 
-    useEffect(() => {
-        localStorage.setItem(KEY_LOCALE, locale)
-    }, [locale])
+  useEffect(() => {
+    localStorage.setItem(KEY_LOCALE, locale);
+  }, [locale]);
 
-    return (
-        <div>
-            <p>
-                <span id="current-time-label">現在時刻</span>
-                <span>:{timestamp.toLocaleString(locale)}</span>
-                <select
-                    title="a"
-                    value={locale}
-                    onChange={(e) => setLocale(getLocaleFromString(e.target.value))}
-                >
-                    <option value="en-US">en_US</option>
-                    <option value="ja_JP">ja_JP</option>
-                </select>
-            </p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>
+        <span id="current-time-label">現在時刻</span>
+        <span>:{timestamp.toLocaleString(locale)}</span>
+        <select
+          title="a"
+          value={locale}
+          onChange={(e) => setLocale(getLocaleFromString(e.target.value))}
+        >
+          <option value="en-US">en-US</option>
+          <option value="ja-JP">ja-JP</option>
+        </select>
+      </p>
+    </div>
+  );
+};
